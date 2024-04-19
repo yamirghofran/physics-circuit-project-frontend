5d3b179fd91bfbbf0664f0f407f8804a2d97fd4e
Yousef Amirghofran <yamirghofran@gmail.com>
2024-04-20 00:31:08 +0200

    General Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
   return (
     <div className='w-full'>
         <h1 className='text-2xl font-semibold text-center mt-6'>The Best Physics Project</h1>
+        <h2 className='text-center text-xl my-2'>Yousef Amirghofran, Anze Zgon, Mia Dragovic, Borja Albers, and Lucas Van Zyl</h2>
         <div className='flex w-full gap-x-4 px-4 mt-6'>
           <PeopleTable />
           <Notifications />
